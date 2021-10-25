bc83b1e8af4bb1a8faafdfcddecd62872ff03ce2
devbotas <simon@devbot.lt>
2021-10-25 10:44:33 +0300

    documenting and fixing a few small things.

--- Lupus/Program.cs
+++ Lupus/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Text;
+using System.Threading;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
@@ -9,16 +11,39 @@ namespace Lupus {
         private static Logger _log = LogManager.GetCurrentClassLogger();
 
         static void Main(string[] args) {
-            NLog.Layouts.Layout localLayout = "${longdate}|${uppercase:${level}}|${message}";
+            // Configuring NLog.
+            NLog.Layouts.Layout localLayout = "${longdate}|${uppercase:${level}}|${message} ${onexception}";
             var config = new LoggingConfiguration();
 
+            var logfile = new FileTarget("file");
+            logfile.Layout = localLayout;
+            logfile.MaxArchiveFiles = 30;
+            logfile.ArchiveDateFormat = "yyyy-MM";
+            logfile.ArchiveNumbering = ArchiveNumberingMode.Date;
+            logfile.ArchiveEvery = FileArchivePeriod.Month;
+            logfile.FileName = "logs/ApplicationLog.txt";
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile, "Lupus.*");
 
+            var logconsole = new ColoredConsoleTarget("console");
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole, "Lupus.*");
+
+            var logdebug = new DebuggerTarget("debugger");
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logdebug, "Lupus.*");
+
+            LogManager.Configuration = config;
+
+            if (Directory.Exists("logs") == false) {
+                Directory.CreateDirectory("logs");
+            }
+
+            _log.Info("Loading ENV variables...");
             var brokerIp = Environment.GetEnvironmentVariable("MQTT_BROKER_IP");
             if (string.IsNullOrEmpty(brokerIp)) {
                 _log.Warn("Evironment variable \"MQTT_BROKER_IP\" is not provided. Using 127.0.0.1.");
                 brokerIp = "127.0.0.1";
             }
 
+            _log.Info("Setting up MQTT broker connection...");
             var channelOptions = new Tevux.Protocols.Mqtt.ChannelConnectionOptions();
             channelOptions.SetHostname(brokerIp);
 
@@ -56,23 +81,8 @@ namespace Lupus {
             logConnection.ConnectAndWait(channelOptions);
             logConnection.Subscribe("tevux/logs/#", Tevux.Protocols.Mqtt.QosLevel.AtLeastOnce);
 
-
-            var logconsole = new ColoredConsoleTarget("console");
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole, "Lupus.*");
-
-            var logdebug = new DebuggerTarget("debugger");
-            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logdebug, "Lupus.*");
-
-            var logfile = new FileTarget("file");
-            logfile.Layout = localLayout;
-            logfile.MaxArchiveFiles = 30;
-            logfile.ArchiveDateFormat = "yyyy-MM";
-            logfile.ArchiveNumbering = ArchiveNumberingMode.Date;
-            logfile.ArchiveEvery = FileArchivePeriod.Month;
-            logfile.FileName = "logs/ApplicationLog.txt";
-            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile, "Lupus.*");
-
-            LogManager.Configuration = config;
+            _log.Info("Initialization completed.");
+            Thread.Sleep(-1);
         }
     }
 }
